537ee9c469120b16614836bb4bf04c691deedb2b
Jonathan Bernard <jdb@jdb-labs.com>
2015-11-15 00:24:24 -0600

    Add support for dependency management with a UMD.
    Make the plugin behave well when used as an AMD or CommonJS module.
    See https://github.com/umdjs/umd for details regarding UMD.

--- jquery.magicmove.js
+++ jquery.magicmove.js
@@ -1,4 +1,32 @@
-(function($){
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($){
   var defaults = {
     selector: '> *',
     visible: { opacity: 1, transform: 'scale(1)' },
@@ -196,4 +224,4 @@
       });
     });
   };
-})(jQuery);
+}));
